856b8532325ec4b82ea987bd4319c9b9cf8a53e2
Frank Su <frank.su@applydigital.co>
2024-07-05 17:11:51 -0700

    Add initial form styling

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,11 +1,45 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, ScrollView, TextInput } from "react-native";
+import { useState } from "react";
+import { Colors } from "../../constants/colors";
 
 function PlaceForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  function changeTitleHandler(enteredText) {
+    setEnteredTitle(enteredText);
+  }
   return (
-    <View>
-      <Text>Place form</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={changeTitleHandler}
+          value={enteredTitle}
+        />
+      </View>
+    </ScrollView>
   );
 }
 
 export default PlaceForm;
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: 4,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100,
+  },
+});
