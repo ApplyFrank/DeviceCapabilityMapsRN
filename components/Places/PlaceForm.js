9c777189dc622afe5144609f98f33beb0f8a683b
Frank Su <frank.su@applydigital.co>
2024-07-09 23:46:06 -0700

    Add location picker UI

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -2,6 +2,7 @@ import { Text, View, StyleSheet, ScrollView, TextInput } from "react-native";
 import { useState } from "react";
 import { Colors } from "../../constants/colors";
 import ImagePicker from "./ImagePicker";
+import LocationPicker from "./LocationPicker";
 
 function PlaceForm() {
   const [enteredTitle, setEnteredTitle] = useState("");
@@ -18,6 +19,7 @@ function PlaceForm() {
           value={enteredTitle}
         />
         <ImagePicker />
+        <LocationPicker />
       </View>
     </ScrollView>
   );
