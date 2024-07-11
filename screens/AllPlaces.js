f747154ed169bc204aa623e767add22125700ba9
Frank Su <frank.su@applydigital.co>
2024-07-10 23:45:25 -0700

    Add ability to list all places proto

--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,5 +1,19 @@
 import PlacesList from "../components/Places/PlacesList";
-function AllPlaces() {
-  return <PlacesList />;
+import { useEffect, useState } from "react";
+import { useIsFocused } from "@react-navigation/native";
+
+function AllPlaces({ route }) {
+  const [loadedPlaces, setLoadedPlaces] = useState([]);
+
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    if (isFocused && route.params) {
+      // NOTE technique to add new elemets to state if state is a function
+      setLoadedPlaces((curPlaces) => [...curPlaces, route.params.place]);
+    }
+  }, [isFocused, route]);
+
+  return <PlacesList places={loadedPlaces} />;
 }
 export default AllPlaces;
