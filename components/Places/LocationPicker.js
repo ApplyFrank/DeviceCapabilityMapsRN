2e1081a7c42a3ea676098c3dfc464887d1c5f5c7
Frank Su <frank.su@applydigital.co>
2024-07-10 20:37:22 -0700

    Previewing picked locations

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -6,15 +6,34 @@ import {
   useForegroundPermissions,
   PermissionStatus,
 } from "expo-location";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { getMapPreview } from "../../util/location";
-import { useNavigation } from "@react-navigation/native";
+import {
+  useNavigation,
+  useRoute,
+  useIsFocused,
+} from "@react-navigation/native";
 
 function LocationPicker() {
   const [pickedLocation, setPickedLocation] = useState();
   const [locationPermissionInformation, requestPermission] =
     useForegroundPermissions();
   const navigation = useNavigation();
+  const route = useRoute();
+  // NOTE when we navigate back from navigation.navigate from a child, the stack screens don't rerender
+  // can use useIfFocused to see if it is focused to work around this
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    if (isFocused && route.params) {
+      const mapPickedLocation = {
+        lat: route.params.pickedLat,
+        lng: route.params.pickedLng,
+      };
+      setPickedLocation(mapPickedLocation);
+    }
+  }, [route, isFocused]);
+
   async function verifyPermissions() {
     if (
       locationPermissionInformation.status === PermissionStatus.UNDETERMINED
